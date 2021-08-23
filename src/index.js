a324eb7803ced7e6aab2b77466390cf48e77ed74
Meghan Sims <mrs.meghansims@gmail.com>
2021-08-23 22:22:07 +0000

    Created lanes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,37 @@
 
 import Phaser from 'phaser';
 //import logoImg from './assets/logo.png';
-import spriteSheetImg from './assets/Frogger.png';
+import spriteSheetImg from './assets/Frog.png';
+
+const WIDTH = 800;
+const HEIGHT = 450;
+const LANE_HEIGHT = 50;
+const WATER_COLOR = 0x0000ff;
+const ROAD_COLOR = 0x303030;
+const GRASS_COLOR = 0x00ff00;
+const LOG_COLOR = 0x964b00;
+const CAR_COLOR = 0xff0000;
+
+const LANES = [
+   // Terrain, Speed, 
+    { terrain: 'grass', speed: 50 }, //Lane 1
+    { terrain: 'grass', speed: 50 }, //Lane 2
+    { terrain: 'road', speed: 50 }, //Lane 3
+    { terrain: 'road', speed: 50 }, //Lane 4
+    { terrain: 'road', speed: 50 }, //Lane 5
+    { terrain: 'water', speed: 50 }, //Lane 6
+    { terrain: 'water', speed: 50 }, //Lane 7
+    { terrain: 'water', speed: 50 }, //Lane 8
+    { terrain: 'lilypads', speed: 50 }, //Lane 9
+];
+
+LANES.forEach(lane => {
+    const obstacles = Array.apply(null, Array(24))
+    lane.obstacles = obstacles.map(obstacle =>{
+        return Math.round(Math.random())
+    })
+}) 
+
 
 class MyGame extends Phaser.Scene
 {
@@ -13,26 +43,31 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-        //this.load.image('logo', logoImg);
-        this.load.image('spriteSheet', spriteSheetImg)
+        //this.load.image('spriteSheet', spriteSheetImg)
     }
       
     create ()
     {
-        // // Add image
-        // this.spriteSheet = this.add.image(0, 0, 'spriteSheet');
-        // //frogPic = this.add.image(50,50,'spriteSheet')
-
-        // cropFrog = new Phaser.
-
-        // spriteSheet.crop(cropFrog)
-
+        // Add image
+        this.spriteSheet = this.add.image(0, 0, 'spriteSheet');
         
-        // // Set initial position
-        // this.spriteSheet.setPosition(0, 0);
+        // Draw Lanes
+        for(var laneNum in LANES){
+            const lane = LANES[laneNum]
+            console.log(laneNum, lane)
 
-        //  // Cursors input
-        // this.cursors = this.input.keyboard.createCursorKeys();
+            var color = GRASS_COLOR;
+            if (lane.terrain === 'water') color = WATER_COLOR;
+            if (lane.terrain === 'road') color = ROAD_COLOR;
+
+            this.add.rectangle(WIDTH/2, HEIGHT-laneNum*LANE_HEIGHT-LANE_HEIGHT/2, WIDTH, LANE_HEIGHT, color);
+        }
+              
+        // Set initial position
+        this.spriteSheet.setPosition(WIDTH/2, HEIGHT/2);
+
+         // Cursors input
+        this.cursors = this.input.keyboard.createCursorKeys();
         
     }
 update ()    
@@ -61,17 +96,17 @@ update ()
          {
              this.logo.x = 0;
          }
-         else if (this.spriteSheet.x > 800)
+         else if (this.spriteSheet.x > WIDTH)
          {
-             this.spriteSheet.x = 800;
+             this.spriteSheet.x = WIDTH;
          }
          else if (this.spriteSheet.y < 0)
          {
              this.spriteSheet.y = 0;
          }
-         else if (this.spriteSheet.y > 600)
+         else if (this.spriteSheet.y > HEIGHT)
          {
-             this.spriteSheet.y = 600;
+             this.spriteSheet.y = HEIGHT;
          }
    }
 }
@@ -81,10 +116,10 @@ update ()
  const config = {
      type: Phaser.AUTO,
      parent: 'phaser-example',
-     width: 800,
-     height: 600 ,
+     width: WIDTH,
+     height: HEIGHT ,
      scene: MyGame,
-     backgroundColor: '#00FF00'
+     backgroundColor: '#000000'
  }
 
  const game = new Phaser.Game(config);
